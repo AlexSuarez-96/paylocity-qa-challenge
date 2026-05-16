d73cae5ccc609a9e5a9f9d963fd6f2c98fd41b92
Alex Suarez <jesuss.96@hotmail.com>
2026-05-16 15:36:25 -0600

    new comments on playwright

--- tests/ui/benefits-dashboard-ui.spec.js
+++ tests/ui/benefits-dashboard-ui.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 const columns = {
+  // Column Mapping
   ID: 0,
   LastName: 1,
   FirstName: 2,
@@ -13,6 +14,7 @@ const columns = {
 };
 
 const expectedData = {
+  //Reference Dataset to validate against the UI//
   ID: "5666feba-60e7-4bb4-8dfb-f649c4f886ed",
   LastName: "Rogers",
   FirstName: "Steve",
@@ -21,17 +23,21 @@ const expectedData = {
 
 test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
   test.beforeEach(async ({ page }) => {
+    // LOG IN located in Before Each to avoid hard code in every test
     await page.goto(
       "https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/LogIn",
     );
 
-    await expect(page.getByLabel("Username")).toBeVisible({ timeout: 10000 });
+    await expect(page.getByLabel("Username")).toBeVisible({ timeout: 10000 }); // expects and timeouts used to
+    // allow resources be fully loaded
+
     await expect(page.getByLabel("Password")).toBeVisible({ timeout: 10000 });
 
     await page.getByLabel("Username").fill("TestUser961");
     await page.getByLabel("Password").fill("O0^G]9Cd8N|k");
 
     await expect(page.getByLabel("Username")).toHaveValue("TestUser961", {
+      // Verify inputs are correctly filled before clicking login
       timeout: 15000,
     });
     await expect(page.getByLabel("Password")).toHaveValue("O0^G]9Cd8N|k", {
@@ -40,21 +46,23 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
 
     await page.getByRole("button", { name: "Log In" }).click();
 
-    await expect(page.getByLabel("Username")).toBeHidden({ timeout: 10000 });
-    await expect(page.getByLabel("Password")).toBeHidden({ timeout: 10000 });
+    await expect(page.getByLabel("Username")).toBeHidden({ timeout: 10000 }); // Assertions/timeouts made to confirm
+    await expect(page.getByLabel("Password")).toBeHidden({ timeout: 10000 }); // correct Log In and handle potential slow connection
 
     await expect(page).toHaveURL(
       "https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Benefits",
     );
 
-    await page.waitForLoadState("networkidle");
+    await page.waitForLoadState("networkidle"); // Ensure page stability and that all resources are loaded
 
     await page.waitForSelector("#employeesTable tbody", {
+      //Wait for the table to be visible and stable
       state: "visible",
       timeout: 20000,
     });
 
     await page.waitForSelector("#employeesTable tbody tr", {
+      //Waiting to content of the table to be visible, avoid empty table
       state: "visible",
       timeout: 20000,
     });
@@ -64,17 +72,18 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
     page,
   }) => {
     const row = page.getByRole(`row`, {
+      //Locate the specific row using the unique ID from expected dataset
       name: expectedData.ID,
     });
 
-    const cells = row.getByRole(`cell`);
+    const cells = row.getByRole(`cell`); //Divide the rows by cells
 
-    const lastNameColumnValue = await cells.nth(columns.LastName).textContent();
-    const firstNameColumnValue = await cells
+    const lastNameColumnValue = await cells.nth(columns.LastName).textContent(); //Save the data storaged in the cells of lastName
+    const firstNameColumnValue = await cells //and FirstName columns of expectedData
       .nth(columns.FirstName)
       .textContent();
 
-    await expect(lastNameColumnValue).toBe(expectedData.LastName);
+    await expect(lastNameColumnValue).toBe(expectedData.LastName); //Compare the values to see if the UI values match against our dataset
     await expect(firstNameColumnValue).toBe(expectedData.FirstName);
 
     console.log(
@@ -95,13 +104,15 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
     await page
       .getByRole(`link`, { name: `Paylocity Benefits Dashboard` })
       .click();
+
+    //Check if we can get unauthorized access to the table
     await expect(page).not.toHaveURL(
       `https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Benefits`,
     );
   });
 
+  //Precondition: created a Frank Castle employee before, we are gonna create a new Frank with the same charcateristics
   test(`B-003 UI - Duplicate records in the Benefits Dashboard`, async ({
-    //Precondition: created a Frank Castle employee before
     page,
   }) => {
     const firstNameOriginal = "Frank";
@@ -110,11 +121,14 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
 
     await expect(page.locator(`table tbody`).first()).toBeVisible();
 
+    // Filtering the table to find all rows that match this specific employee
     const duplicateRows = page
       .getByRole(`row`)
       .filter({ hasText: firstNameOriginal })
       .filter({ hasText: secondNameOriginal })
       .filter({ hasText: dependantsOriginal });
+
+    // Getting the total count of matching records
     const rowCounter = await duplicateRows.count();
 
     if (rowCounter > 1) {
@@ -123,7 +137,7 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
       );
     }
 
-    await expect(rowCounter).toBe(1);
+    await expect(rowCounter).toBe(1); //Verify it only exist one record for the specified employee
   });
 
   test(`B-004 UI - Missing error message / alert in First Name / Last Name fields`, async ({
@@ -131,6 +145,7 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
   }) => {
     await page.getByRole(`button`, { name: `Add Employee` }).click();
 
+    //Boundary Value Test: Testing the UI with a long name
     const longFirstName =
       "Pablo Diego José Francisco de Paula Juan Nepomuceno Crispín Crispiniano María Remedios de la Santísima Trinidad";
     const longLastName = "Ruiz y Picasso";
@@ -138,7 +153,7 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
 
     await page
       .locator(`input[name="firstName"]`)
-      .pressSequentially(longFirstName); // substitution of ".fill" for ".pressSequentially"
+      .pressSequentially(longFirstName); // substitution of ".fill" for ".pressSequentially" to simulate human interaction
     await page
       .locator(`input[name="lastName"]`)
       .pressSequentially(longLastName);
@@ -147,6 +162,8 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
       .pressSequentially(dependantsOfLongName);
     await page.locator(`#addEmployee`).click();
 
+    //Test to see if the employee was correctly added to our table
+    // Test expected to fail due to the API Contract maxLength = 50 and UI fails to show an error message
     await expect(page.getByRole(`row`, { name: longFirstName })).toBeVisible();
   });
 
@@ -155,6 +172,7 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
   }) => {
     await page.getByRole(`button`, { name: `Add Employee` }).click();
 
+    //Boundary Value Test: Testing the UI with big quantity of dependants
     const bigDependantFirstName = "Valentina";
     const bigDependantLastName = "Vassilyeva";
     const bigDependant = "69";
@@ -170,13 +188,15 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
       .pressSequentially(bigDependant);
     await page.locator(`#addEmployee`).click();
 
+    // Test expected to fail due to the API Contract dependants maximum = 32 and UI fails to show an error message
     await expect(
       page.getByRole(`row`, { name: bigDependantFirstName }),
     ).toBeVisible();
   });
 
+  // Test to simulate user away from keyboard, set a Timeout to simulate that condition
   test(`B-006 UI - Session Management Timeout Blocker`, async ({ page }) => {
-    test.setTimeout(15 * 60 * 1000);
+    test.setTimeout(15 * 60 * 1000); // Set a timeout margin of 15 min to interact with UI and simulate afk of 12 min
 
     await page.getByRole(`button`, { name: `Add Employee` }).click();
 
@@ -199,7 +219,9 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
       .pressSequentially(firstDependantsSM);
     await page.locator(`#addEmployee`).click();
 
-    await page.waitForTimeout(12 * 60 * 1000);
+    await page.waitForTimeout(12 * 60 * 1000); //Simulation of user being away 12 min.
+    //deleting cookies would prevent UI interaction for the subsequent steps
+    //12 min because idk the exact session timeout and i test it with black box method
 
     const addEmployeeBtn = page.getByRole(`button`, { name: `Add Employee` });
     await expect(addEmployeeBtn).toBeVisible({ timeout: 10000 });
@@ -217,12 +239,14 @@ test.describe(`Test Suite for Paylocity Benefits Dashboard`, () => {
       .locator(`input[name="dependants"]`)
       .pressSequentially(secondDependantSM);
 
-    await page.locator(`#addEmployee`).click({ force: true });
+    await page.locator(`#addEmployee`).click({ force: true }); //Force the click to test the UI response
 
+    //Test expected to fail due to new data doesnt get show in the table
     await expect(page.getByRole(`row`, { name: `Talia` })).toBeVisible({
       timeout: 5000,
     });
   });
+
   test(`Happy Path: Add Employee`, async ({ page }) => {
     await page.getByRole(`button`, { name: `Add Employee` }).click();
 
